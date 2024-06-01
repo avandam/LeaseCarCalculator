7e9c98d31ba3c4900390a197af05afd46e692150
Alexander van Dam <avandam@gmail.com>
2024-06-01 14:52:43 +0200

    Added Basic Application

---
+++ LeaseCarCalculator/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace LeaseCarCalculator;
+;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+    private void RaisePropertyChanged(string propName)
+    {
+        PropertyChanged(this, new PropertyChangedEventArgs(propName));
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
